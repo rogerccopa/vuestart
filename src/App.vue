1be41434db71ad8e4e180104ae9a6f51a1a879c6
rogerccopa <rogerccopa@gmail.com>
2024-11-04 16:30:16 -0500

    start composition api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <script>
-export default {
-	data() {
-		return {
-			name: "Roger Ccopa",
-			status: 'active',
-			tasks: ['task one', 'task two', 'task three'],
-			link: 'https://google.com'
+	export default {
+		setup() {
+			const name = "Roger Ccopa Illapuma";
+			const status = "active";
+			const tasks = ["task one", "task two", "task 3"];
+
+			const toggleStatus = () => {
+				if (this.status === "active") {
+					this.status = "pending";
+				} else if (this.status === "pending") {
+					this.status = "inactive";
+				} else {
+					this.active = "active";
+				}
+			};
+
+			return {
+				name,
+				status,
+				tasks,
+				toggleStatus
+			};
 		}
-	}
-}
+	};
 </script>
 <template>
 	<h1>{{ name }}</h1>
-	<br/>
+	<br />
 	<p v-if="status === 'active'">User is active</p>
 	<p v-else-if="status === 'pending'">user is pending</p>
 	<p v-else>User Is INACTIVE</p>
-	
+
+	TASKS
 	<ul>
-		<li v-for="task in tasks" :key="task">{{task}}</li>
+		<li v-for="task in tasks" :key="task">{{ task }}</li>
 	</ul>
-	<!-- <a v-bind:href="link">Go to Google</a> -->
-	<a :href="link">Go to Google site</a>
 </template>
---
+++ src/AppOptions.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+	data() {
+		return {
+			name: "Roger Ccopa",
+			status: 'active',
+			tasks: ['task one', 'task two', 'task three'],
+			link: 'https://google.com'
+		}
+	}
+}
+</script>
+<template>
+	<h1>{{ name }}</h1>
+	<br/>
+	<p v-if="status === 'active'">User is active</p>
+	<p v-else-if="status === 'pending'">user is pending</p>
+	<p v-else>User Is INACTIVE</p>
+	
+	<ul>
+		<li v-for="task in tasks" :key="task">{{task}}</li>
+	</ul>
+	<!-- <a v-bind:href="link">Go to Google</a> -->
+	<a :href="link">Go to Google site</a>
+</template>
