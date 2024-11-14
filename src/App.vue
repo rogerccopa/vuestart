17f45e613be4aa715643a2bf395e6525b4cd5356
Roger Ccopa <rogerccopa@delllaptop.com>
2024-11-13 20:54:14 -0500

    composition API short version

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
-<script>
-import {ref} from 'vue';
+<script setup>
+import { ref } from 'vue';
 
-export default {
-	setup() {
-		const name = ref("Roger Ccopa Illapuma");
-		const status = ref("active");
-		const tasks = ref(["task one", "task two", "task 3"]);
+const name = ref("Roger Ccopa Illapuma");
+const status = ref("active");
+const tasks = ref(["task one", "task two", "task 3"]);
 
-		const toggleStatus = () => {
-			if (status.value === "active") {
-				status.value = "pending";
-			} else if (status.value === "pending") {
-				status.value = "inactive";
-			} else {
-				status.value = "active";
-			}
-		};
-
-		return {
-			name,
-			status,
-			tasks,
-			toggleStatus
-		}
+const toggleStatus = () =>
+{
+	if (status.value === "active")
+	{
+		status.value = "pending";
+	} else if (status.value === "pending")
+	{
+		status.value = "inactive";
+	} else
+	{
+		status.value = "active";
 	}
 };
 </script>
+
 <template>
 	<h1>{{ name }}</h1>
 	<br />
@@ -37,7 +31,7 @@ export default {
 	<ul>
 		<li v-for="task in tasks" :key="task">{{ task }}</li>
 	</ul>
-	<br/><br/>
+	<br /><br />
 
 	<button @click="toggleStatus()">
 		Toggle Status
